b581a50c3f678b29a56d640ab0649605bb9afe54
Romeu <39237217+romeuesteve@users.noreply.github.com>
2022-11-07 00:00:15 +0100

    Update permutacion.hh

--- permutacion.hh
+++ permutacion.hh
@@ -9,23 +9,13 @@ using namespace std;
 
 class Permutacion {
 	private:
-		//Permutacion
 		BinTree<char> crear_arbol(const string& s);
 		string leer_arbol(const BinTree<char> t);
-
-		//Sustitucion
-		//char codificar_char(Alfabeto alfabeto, const char& a, const char& b);
-		//char descodificar_char(Alfabeto alfabeto, const char& c, const char& b);
 	public:
 		Permutacion();
 		~Permutacion();
 
-		//Permutacion
 		void codificar_permutacion(string& s, const int& b);
-
-		//Sustitucion
-		//void codificar_sustitucion(const Alfabeto& alfabeto, const string& t, const string& c);
-		//void descodificar_sustitucion(const Alfabeto& alfabeto, const string& e, const string& c);
 };
 
 #endif
